411a8ff9a39b9fec266b69a2d5e83da783591096
Damien-Senechal <60384944+Damien-Senechal@users.noreply.github.com>
2022-03-14 15:32:26 +0100

    add tilemaps

--- game.js
+++ game.js
@@ -23,8 +23,25 @@ const config = {
 
 const game = new Phaser.Game(config);
 
-function preload() { }
+function preload() {
+  this.load.image('background', 'assets/images/background.png');
+  this.load.image('spike', 'assets/images/spike.png');
+  // At last image must be loaded with its JSON
+  this.load.atlas('player', 'assets/images/kenney_player.png','assets/images/kenney_player_atlas.json');
+  this.load.image('tiles', 'assets/tilesets/platformPack_tilesheet.png');
+  // Load the export Tiled JSON
+  this.load.tilemapTiledJSON('map', 'assets/tilemaps/level1.json');
+}
 
-function create() { }
+function create() {
+  const backgroundImage = this.add.image(0, 0,'background').setOrigin(0, 0);
+  backgroundImage.setScale(2, 0.8);
+
+  const map = this.make.tilemap({ key: 'map' });
+
+  const tileset = map.addTilesetImage('kenny_simple_platformer', 'tiles');
+
+  const platforms = map.createStaticLayer('Platforms', tileset, 0, 200);
+}
 
 function update() { }
